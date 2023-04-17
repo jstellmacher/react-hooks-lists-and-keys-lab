e6807cc6db9a1b140cd93db5068257c69b24d7d2
jai <jaijai.stell@gmail.com>
2023-04-16 17:49:47 -0700

    review again complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+// ! Alrighty so in this below, I am creating a variable anchorLinks inside this variable I iterate over the array links ie. home, about, projects and somehow pass the parameter/ prop link (double check that) then in my <a> tag I have a key and a href. 
+// ! I create a key per part in the list/array that I have ----------- have morgan clarify
+  const anchorLinks = links.map((link)=> (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
+  // ! above the code is now dynamic and destructured (if I understand that correctly) links.map refers to the object. then we are assigning that value to the <a> tag where it says link
+    return <nav>{anchorLinks}</nav>
+  // return <nav>
+  //   <a href="#home">home</a>
+  //   <a href="#about">about</a>
+  //   <a href="#projects">projects</a>
+  // </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,18 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  // ! I have to map through project - In my const projectItems I am creating keys and IDs per item by using map for each project, it creates an id
+  const projectItems = projects.map(project => 
+    <ProjectItem
+    {... project}
+    key = {project.id}
+    />
+    );
+//! Below I am rendering projectItems in my div with the id project list --- 
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectItems}</div>
     </div>
   );
 }
